5d7e8b58965a7ab17db98216c4e445ced246870e
heccberte <dimavdovkin98@mail.ru>
2024-01-11 17:57:32 +0300

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:heccberte/VUE-Fishing-game.git master:gh-pages
+git push -f git@github.com:heccberte/VUE-Fishing-Game.git master:gh-pages
 
 cd -
