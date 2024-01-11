b497b700834097ac66c8697596b27a00bd1b218c
heccberte <dimavdovkin98@mail.ru>
2024-01-11 16:40:41 +0300

    map fix icons

--- src/components/FishingMap.vue
+++ src/components/FishingMap.vue
@@ -1,9 +1,30 @@
 <template>
 	<div class="main">
-			<router-link id="river" class="map__point" to="/russia">
+		<div class="map__location" id="russia">
+			<div class="map__title">
+				<p>Россия</p>
+				<p class="local__price">1кг = 10руб</p>
+			</div>
+			<router-link class="map__point" to="/russia">
 			</router-link>
-			<router-link id="winter" class="map__point" to="/canada"></router-link>
-			<router-link id="africa" class="map__point" to="/africa"></router-link>
+		</div>
+		<div class="map__location" id="canada">
+			<div class="map__title">
+				<p>Канада</p>
+				<p class="local__price">1кг = 15руб</p>
+			</div>
+			<router-link class="map__point" to="/canada">
+			</router-link>
+		</div>
+		<div class="map__location" id="africa">
+			<div class="map__title">
+				<p>Африка</p>
+				<p class="local__price">1кг = 20руб</p>
+			</div>
+			<router-link class="map__point" to="/africa">
+			</router-link>
+		</div>
+
 	</div>
 </template>
 <script>
@@ -13,10 +34,40 @@ export default {
 }
 </script>
 <style>
-
-	.map__point {
-		cursor: pointer;
+	.map__location {
+		height: fit-content;
+		display: flex;
 		position: absolute;
+		flex-direction: column;
+		align-items: center;
+		color: #fff;
+		font-size: 22px;
+		text-align: center;
+	}
+	.map__title {
+		background: #00000089;
+		padding: 2px 5px;
+	}
+
+	.map__location#russia {
+		right: 5px;
+		top: 180px;
+	}
+	.map__location#canada {
+		top: 140px;
+		left: 30px;
+	}
+	.map__location#africa {
+		bottom: 280px;
+		right: 100px;
+	}
+	.local__price {
+		font-size: 12px;
+	}
+	.map__point {
+		margin-top: 10px;
+		cursor: pointer;
+		position: relative;
 		height: 30px;
 		width: 30px;
 		background-image: url('../img/mapPoint.png');
@@ -25,55 +76,7 @@ export default {
 		transition: all .3s;
 		animation: bounce 1.2s infinite;
 	}
-	.map__point#river {
-		top: 250px;
-		right: 20px;
-	}
-	.map__point#river::before {
-		content: 'Россия';
-		font-size: 22px;
-		padding: 2px 5px;
-		background: #00000088;
-		position: relative;
-		top: -35px;
-		color: #fff;
-		left: -27px;
-	}
-	.map__point#winter {
-		top: 160px;
-		left: 130px;
 
-	}
-	.map__point#winter::before {
-		content: 'Канада';
-		font-size: 22px;
-		padding: 2px 5px;
-		background: #00000088;
-		position: relative;
-		top: -35px;
-		color: #fff;
-		left: -27px;
-	}
-
-	.map__point#africa {
-		bottom: 280px;
-		right: 60px;
-
-	}
-	.map__point#africa::before {
-		content: 'Африка';
-		font-size: 22px;
-		padding: 2px 5px;
-		background: #00000088;
-		position: relative;
-		top: -35px;
-		color: #fff;
-		left: -27px;
-	}
-
-	.map__point:hover {
-		transform: scale(1.5);
-	}
 
 	@keyframes bounce {
         0%   { transform: translateY(0); }
